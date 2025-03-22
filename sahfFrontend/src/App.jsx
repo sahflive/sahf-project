5aaed983395e0d86ecfef1a4311e2df0b25b696d
sahflive <sahfwtofficial@gmail.com>
2025-03-22 20:49:34 +0530

    fixing error

--- sahfFrontend/src/App.jsx
+++ sahfFrontend/src/App.jsx
@@ -3,12 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home';
 
 function App() {
-<<<<<<< HEAD
-  const [count, setCount] = useState(0);
 
-=======
- 
->>>>>>> efae85813138ae472d58462de7783707afea4595
   return (
     <>
        <Home/>
